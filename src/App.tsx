2a3c1966030ed55beeae27b6c4d2480078672b70
Darián López Utra <daroxs95@gmail.com>
2024-06-28 03:50:01 +0200

    Improve kitchen-sink app

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
-import { Box, Input, Reset, Button, Paragraph, Flex, Heading } from "./lib";
 import { useState } from "react";
 import { Login } from "./Login";
+import { Jobs } from "./JobsList.tsx";
+import { JobPost } from "./JobPost.tsx";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(false);
 
   if (!loggedIn) return <Login onLogin={() => setLoggedIn(true)} />;
+  if (!selectedPost) return <Jobs onClick={() => setSelectedPost(true)} />;
 
-  return (
-    <Box bg="background-primary-strong" height="100vh">
-      <Flex vertical align="center">
-        <Box width="500px">
-          <Heading>Text</Heading>
-          <Paragraph>
-            <Paragraph inline bold>
-              Lorem
-            </Paragraph>{" "}
-            ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            consectetur, ligula eget tincidunt facilisis, nunc ex ultricies
-            purus, nec eleifend purus felis quis metus. Nullam nec eros nec
-            ligula fermentum consectetur. Donec id risus ac erat ultricies
-            ultricies. Suspendisse potenti. Nulla facilisi. Nulla facilisi.
-            Nulla facilisi.
-          </Paragraph>
-          <Flex vertical>
-            <Reset />
-            <Input label="Email" />
-            <Box>
-              <Button>Button</Button>
-            </Box>
-          </Flex>
-        </Box>
-      </Flex>
-    </Box>
-  );
+  return <JobPost onBack={() => setSelectedPost(false)} />;
 }
 
 export default App;
